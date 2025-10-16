71dffdfd32788348e9f082330f773dba641ac1f3
N.Mitsunaga <n.mitsunaga@churadata.okinawa>
2025-10-16 11:45:41 +0900

    feat: ログインボタンを押された際にログイン処理を実行するためのサーバーアクション

---
+++ src/lib/actions/authenticate.ts
@@ -0,0 +1,24 @@
+// ログイン処理を実行するためのサーバーアクション（auth.tsのsignIn関数を呼び出して認証ロジックを実行させるためのファイル）
+'use server'
+
+import { signIn } from '@/auth'
+import { AuthError } from 'next-auth'
+
+export async function authenticate(
+	prevState: string | undefined,
+	formData: FormData,
+) {
+	try {
+		await signIn('credentials', formData);
+	} catch (error) {
+		if (error instanceof AuthError) {
+			switch (error.type) {
+				case 'CredentialsSignin':
+					return 'メールアドレスまたはパスワードが正しくありません';
+				default:
+					return 'エラーが発生しました。';
+			}
+		}
+		throw error;
+	}
+}
