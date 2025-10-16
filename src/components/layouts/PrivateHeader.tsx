2eb587f9091818099822d8de6638abd9e1ea52bb
N.Mitsunaga <n.mitsunaga@churadata.okinawa>
2025-10-16 11:48:58 +0900

    feat: プライベートヘッダーの作成とそのヘッダー上で表示させるドロップダウンメニューを作成

---
+++ src/components/layouts/PrivateHeader.tsx
@@ -0,0 +1,29 @@
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList } from "@/components/ui/navigation-menu"
+import Link from "next/link";
+import Setting from "./Setting";
+import { auth, signOut } from "@/auth";
+
+export default async function PrivateHeader() {
+	const session = await auth();
+	if (!session?.user?.email) throw new Error("不正なリクエストです")
+
+	return (
+		<header className="border-b bg-blue-200">
+			<div className="container mx-auto px-4 py-4 flex items-center justify-between">
+				<NavigationMenu>
+					<NavigationMenuList>
+						<NavigationMenuItem>
+							{/* 本来、shadcn/uiのNavigationMenuLinkとNext.jsのLinkを併用すると<a>タグが二重で生成されてしまうため、asChildを使用して<a>タグを１つにマージする。 これによりshadcn/uiのNavigationMenuLinkの機能を利用しつつ、Next.jsのLinkの機能（リンク先のプリフェッチ）を利用できる。 */}
+							<NavigationMenuLink asChild>
+								<Link href="/dashboard" className="font-bold text-xl">
+									管理ページ
+								</Link>
+							</NavigationMenuLink>
+						</NavigationMenuItem>
+					</NavigationMenuList>
+				</NavigationMenu>
+				<Setting session={session} />
+			</div>
+		</header>
+	)
+}
