fba6113281d02993377d62342ab1b61a78b32585
N.Mitsunaga <n.mitsunaga@churadata.okinawa>
2025-10-21 14:37:23 +0900

    feat: ログインしているユーザーが投稿した記事かつ指定した記事を取得するためのORM記述（詳細ページ用）

--- src/lib/ownPost.ts
+++ src/lib/ownPost.ts
@@ -1,18 +1,22 @@
 import { prisma } from "@/lib/prisma";
 
-export async function getOwnPosts(userId: string) { // ログインしているユーザーのIDを引数に受ける（sessionから取得する）
-	return await prisma.post.findMany({
+export async function getOwnPost(userId: string, postId: string) {
+	return await prisma.post.findFirst({
 		where: {
-			authorId: userId
+			AND: [
+				{ authorId: userId },
+				{ id: postId }
+			]
 		},
 		select: {
 			id: true,
 			title: true,
+			content: true,
+			topImage: true,
+			author: true, // postsテーブルとrelation関係であるusersテーブルのデータをjoinして取得
 			published: true,
+			createdAt: true,
 			updatedAt: true,
-		},
-		orderBy: {
-			updatedAt: "desc"
 		}
 	})
 }
