0a4a0ce3ee92573787b7d4f79d76e9bf15c51c7e
N.Mitsunaga <n.mitsunaga@churadata.okinawa>
2025-10-25 16:06:44 +0900

    feat: supabaseに画像を保存するパターンとローカルに画像を保存する２パターンを用意

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -2,8 +2,20 @@
 
 import { writeFile } from "fs/promises";
 import path from "path";
+import { supabase } from "@/lib/supabase";
 
 export async function saveImage(file: File): Promise<string | null> {
+	const useSupabase = process.env.NEXT_PUBLIC_USE_SUPABASE_STORAGE === "true";
+
+	if (useSupabase) {
+		return await saveImageToSupabase(file);
+	} else {
+		return await saveImageToLocal(file);
+	}
+
+}
+
+export async function saveImageToLocal(file: File): Promise<string | null> {
 	const buffer = Buffer.from(await file.arrayBuffer()) // 画像ファイルのarrayBufferを取り出してBufferに変換
 	const fileName = `${Date.now()}_${file.name}`; // 保存する画像ファイル名を作成
 	const uploadDir = path.join(process.cwd(), "public/images"); // 画像をアップロードするフォルダパスを作成
@@ -16,4 +28,28 @@ export async function saveImage(file: File): Promise<string | null> {
 		console.error("画像保存エラー:", error);
 		return null;
 	}
+}
+
+export async function saveImageToSupabase(file: File): Promise<string | null> {
+	const fileName = `${Date.now()}_${file.name}`;
+	const bucketName = process.env.NEXT_PUBLIC_SUPABASE_BUCKET_NAME!;
+	const { data, error } = await supabase.storage.from(bucketName).upload(fileName, file, {
+		cacheControl: "3600",
+		upsert: false
+	});
+
+	if (error) {
+		console.error("Upload failed:", error.message);
+		return null
+	}
+
+	// 返り値の例
+	// {
+	//   data: {
+	//     publicUrl: "https://xxx/storage/v1/object/public/images/photo.jpg"
+	//   }
+	// }
+	const res = supabase.storage.from(bucketName).getPublicUrl(fileName); // supabaseのストレージの指定のバケットにアクセスして、指定したファイル名のパスを取得して、publicUrlDataに格納する。このとき、dataという変数で
+
+	return res.data.publicUrl;
 }
