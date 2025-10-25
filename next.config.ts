3388c88e80798ce7e794e53e62d5ffc79cf6f661
N.Mitsunaga <n.mitsunaga@churadata.okinawa>
2025-10-25 16:06:11 +0900

    feat: supabaseのバケットに保存されている画像にアクセスできるようにnextのconfigを修正

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // 外部の画像を扱う場合の設定
+  // nextが用意しているImageコンポーネントで外部URLの画像を使用する場合は、許可する外部の画像のURLを設定する必要がある。（悪意のあるサイトからの画像読み込みを防止するため）
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "picsum.photos",
       },
+      {
+        protocol: "https",
+        hostname: "gvqbifuiqchkteqequut.supabase.co" // supabaseに保存された画像を扱うため設定する
+      }
     ],
   },
   experimental: {
